c26c5b209f18932e7983fbe4964e3e8efbd3661d
Treybrowder <tbrowder7@gmail.com>
2024-01-28 22:25:29 -0500

    initial set up & adding gitignore

---
+++ IMChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMChat
+//
+//  Created by Trey Browder on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
