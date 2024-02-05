3753e96e6122e9e7beede2314e41d38828b7fbea
Treybrowder <tbrowder7@gmail.com>
2024-02-05 13:54:13 -0500

    added dependencies need to hook up firebase for login and registration of accounts

---
+++ Resources/extensions.swift
@@ -0,0 +1,38 @@
+//
+//  extensions.swift
+//  IMChat
+//
+//  purpose:
+//      - add comuputed properties to avoid redundancy
+//
+//  Created by Trey Browder on 1/29/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
